443654d2c80354018df118925e1548d59b8372e8
ytian90 <yu.tian@cix.csi.cuny.edu>
2015-09-10 13:51:58 -0400

    WSGI App

--- hello.py
+++ hello.py
@@ -15,6 +15,7 @@ settings.configure(
 )
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application # WSGI application
 from django.http import HttpResponse
 
 # Creating the View
@@ -25,6 +26,9 @@ def index(request):
 urlpatterns = (
         url(r'^$', index),
 )
+
+# WSGI Application
+application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
